12ab205ce42d9dcff52296782a2947fdc7ab3a81
Grant Watson <gaw922@gmail.com>
2022-09-01 12:19:46 -0500

    Built out rest of app's features

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
+    
+    var colors: [UIColor] = []
+    
+    enum Cells {
+        static let colorCell = "ColorCell"
+    }
+    
+    enum Segues {
+        static let toDetail = "toColorDetailVC"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addRandomColors()
+    }
+    
+    func addRandomColors() {
+        for _ in 0 ..< 50 {
+            colors.append(UIColor.random())
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! ColorDetailVC
+        destVC.color = sender as? UIColor
     }
 }
 
@@ -20,14 +40,20 @@ class ColorsTableVC: UIViewController {
 extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.colorCell) else {
+            return UITableViewCell()
+        }
+        
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "toColorDetailVC", sender: nil)
+        let color = colors[indexPath.row]
+        performSegue(withIdentifier: Segues.toDetail, sender: color)
     }
 }
---
+++ RandomColors/UIColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtension.swift
+//  RandomColors
+//
+//  Created by Grant Watson on 9/1/22.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func random() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+        return randomColor
+    }
+}
