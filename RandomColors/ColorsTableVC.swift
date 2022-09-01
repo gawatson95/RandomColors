92e317055191d694ea1fb78aca7de6014259cf5b
Grant Watson <gaw922@gmail.com>
2022-08-31 21:38:18 -0500

    Playgrounding with UIKit

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -14,19 +14,20 @@ class ColorsTableVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+}
+
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
-
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "toColorsDetailVC", sender: nil)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toColorDetailVC", sender: nil)
+    }
 }
